988f9fcd3015837a2c74cc174ec038ea5a532479
Juan Cadavid <jmcada@amazon.com>
2026-02-17 19:42:51 -0600

    Updated usage of global NvimKiroPlugin to simplify

--- lua/nvim-kiro/init.lua
+++ lua/nvim-kiro/init.lua
@@ -1,36 +1,11 @@
-local main = require("nvim-kiro.main")
 local conf = require("nvim-kiro.config")
 
-local NvimKiroPlugin = {}
-
---- Toggle the plugin by calling the `enable`/`disable` methods respectively.
-function NvimKiroPlugin.toggle()
-    if _G.NvimKiroPlugin.config == nil then
-        _G.NvimKiroPlugin.config = conf.options
-    end
-
-    main.toggle("public_api_toggle")
-end
-
---- Initializes the plugin, sets event listeners and internal state.
-function NvimKiroPlugin.enable(scope)
-    if _G.NvimKiroPlugin.config == nil then
-        _G.NvimKiroPlugin.config = conf.options
-    end
-
-    main.toggle(scope or "public_api_enable")
-end
-
---- Disables the plugin, clear highlight groups and autocmds, closes side buffers and resets the internal state.
-function NvimKiroPlugin.disable()
-    main.toggle("public_api_disable")
-end
+local M = {}
 
 -- setup NvimKiroPlugin options and merge them with user provided ones.
-function NvimKiroPlugin.setup(opts)
+function M.setup(opts)
+    _G.NvimKiroPlugin = _G.NvimKiroPlugin or {}
     _G.NvimKiroPlugin.config = conf.setup(opts)
 end
 
-_G.NvimKiroPlugin = NvimKiroPlugin
-
-return _G.NvimKiroPlugin
+return M
